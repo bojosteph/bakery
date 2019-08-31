5dfa54c906559f12d2ed8972f18bcaf433957d6f
Roberto Samia <bojosteph@gmail.com>
2019-08-31 13:48:16 -0400

    added category category_products and session model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_31_173107) do
+ActiveRecord::Schema.define(version: 2019_08_31_174634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,20 @@ ActiveRecord::Schema.define(version: 2019_08_31_173107) do
   create_table "cart_items", force: :cascade do |t|
     t.integer "product_id", null: false
     t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
+  end
+
+  create_table "category_products", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -41,6 +55,13 @@ ActiveRecord::Schema.define(version: 2019_08_31_173107) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["title"], name: "index_products_on_title", unique: true
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_token", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
